5e51d8935508a69f44d32e30865876a3f513437b
hiroyuki_miyashita <hiroyuki_miyashita@pokelabo.co.jp>
2021-02-23 15:31:00 +0900

    study Component

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 //関数型コンポーネント Functional Component
+//アロー関数で記述
 //Articleという関数を作る
+//propsを引数にする
+//jsxを返却する
+//statelessであることが特徴
+//シンプル！！！
+//最近はこちらのFunctionalComponentを利用したほうがいい。Componentoにstateを持つべきではない、さらにFunctionComponentがシンプルなので
+
 const Article = (props) => {
+    // constructor(props){
+    //     super(props);
+    // }
     let src ="";
     if (props.isPublished) {
-src="公開";
+        src="公開";
+    } else {
+        src="未公開";
+
     }
     //return でjsxを返却
     return (
@@ -18,5 +31,37 @@ src="公開";
             </div>
     )
 };
+
+
+//↓こっちでもうごく
+//Class Component
+//ライフサイクルやstatefullであることが特徴
+//React.Component を継承
+//propsにはthisが必要 クラス内のpropsを利用するということで
+//render メソッド内でJSXをreturnする
+//複雑だけど 状態(state)を持つことができる コンポーネントが作られたときや、廃棄されたときのstateの関数を呼び出せれる。
+/*
+class Article extends React.Component {
+    constructor(props){
+        super(props);
+    }
+//     let src ="";
+//     if (props.isPublished) {
+// src="公開";
+//     }
+    //return でjsxを返却
+    render() {
+        return (
+        <div>
+            <h2>{this.props.title}</h2>
+    <p>順番は{this.props.order}</p>
+    <p>著者:{this.props.author}</p>
+            </div>
+            
+
+        );
+    }
+};
+*/
 
 export default Article;
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Article from './Article';
 
+//Class Component
+//ライフサイクルやstatefullであることが特徴
+//React.Component を継承
+//propsにはthisが必要 クラス内のpropsを利用するということで
+//render メソッド内でJSXをreturnする
 class Blog extends React.Component {
     constructor(props) {
         super(props);
@@ -8,10 +13,14 @@ class Blog extends React.Component {
     render() {
         const authorName="Miyashita";
         return (
-            <React.Fragment>
+            //React.Fragmentｊは省略可能
+            //order , isPublishing , authorはArticleの引数
+            //コンポーネントは再利用が可能
+            <>
+            {/* <React.Fragment> */}
                 <Article title={"reactの使い方"}
                 order ={3}
-                isPublished={true}
+                isPublished={false}
                 author={authorName}
                 />
                 <Article title={"222reactの使い方"}
@@ -19,7 +28,8 @@ class Blog extends React.Component {
                 isPublished={true}
                 author={authorName}
                 />
-            </React.Fragment>
+            {/* </React.Fragment> */}
+            </>
         )
     }
 }
